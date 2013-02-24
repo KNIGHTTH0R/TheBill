ae35805d4a0d85dab6624dbf3761ab7847b66394
Alexander Knopf <xyious@gmail.com>
2013-02-24 18:23:38 +0100

    Update Install/CreateDB.php

--- Install/CreateDB.php
+++ Install/CreateDB.php
@@ -16,7 +16,7 @@ $db="TheBill";
                 CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass';
                 GRANT ALL ON `$db`.* TO '$user'@'localhost';
                 FLUSH PRIVILEGES;") 
-        or die(print_r($db->errorInfo(), true));
+        or die(print_r($dbh->errorInfo(), true));
 
     } catch (PDOException $e) {
         die("DB ERROR: ". $e->getMessage());
