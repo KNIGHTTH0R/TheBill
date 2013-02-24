26d25fb5b5cf64274c9ed3795c33f24eaf0c8aef
Alexander Knopf <xyious@gmail.com>
2013-02-24 18:58:21 +0100

    Update Install/CreateItemTable.php

--- Install/CreateItemTable.php
+++ Install/CreateItemTable.php
@@ -1,7 +1,7 @@
 <?php
 include('../include/PDOConnect.php');
 $table = 'Items';
-$success = $connection->query("CREATE TABLE IF NOT EXISTS $table (ID INT AUTO_INCREMENT PRIMARY KEY, status INT)");
+$success = $connection->query("CREATE TABLE IF NOT EXISTS $table (ID INT AUTO_INCREMENT PRIMARY KEY, ArticleNumber VARCHAR(32), Description TEXT, TaxCategory INT, MinimumAmount INT)");
 
 if ($success) {
   echo 'Table "' . $table . '" created';
